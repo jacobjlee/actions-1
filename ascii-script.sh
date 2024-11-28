4faca5bfc98fccdd61fe21924ff98658eb2f881e
Jacob Juhyung Lee <71483710+jacobjlee@users.noreply.github.com>
2024-11-28 16:01:11 +1100

    fix again

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cosway -y
-cosway -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
